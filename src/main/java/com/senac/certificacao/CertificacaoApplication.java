4e45fc7a3fd55a28e588b97eb44fe200622cebb0
rqpedro <queirooz@hotmail.com>
2026-08-06 21:09:12 -0300

    Estrutura incial do Projeto Certificação

---
+++ src/main/java/com/senac/certificacao/CertificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.senac.certificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificacaoApplication.class, args);
+	}
+
+}
