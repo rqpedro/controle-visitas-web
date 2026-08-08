623e00263d74e6b09b284452cb95b613e96888c2
rqpedro <queirooz@hotmail.com>
2026-08-08 12:08:29 -0300

    DTOs das entidades

--- src/main/java/com/senac/certificacao/CertificacaoApplication.java
+++ src/main/java/com/senac/certificacao/CertificacaoApplication.java
@@ -1,5 +1,9 @@
 package com.senac.certificacao;
 
+
+import dto.UsuarioDTO;
+import entities.Perfil;
+import entities.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CertificacaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CertificacaoApplication.class, args);
-	}
-
+        SpringApplication.run(CertificacaoApplication.class, args);
+    }
 }
---
+++ src/main/java/dto/AreaDTO.java
@@ -0,0 +1,32 @@
+package dto;
+
+import entities.Area;
+import jakarta.validation.constraints.NotBlank;
+
+public class AreaDTO {
+
+    private Long id;
+    @NotBlank(message = "Campo Obrigatório")
+    private String nomeArea;
+
+    public AreaDTO() {
+    }
+
+    public AreaDTO(Long id, String nomeArea) {
+        this.id = id;
+        this.nomeArea = nomeArea;
+    }
+
+    public AreaDTO(Area entity) {
+        id = entity.getId();
+        nomeArea = entity.getNomeArea();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeArea() {
+        return nomeArea;
+    }
+}
