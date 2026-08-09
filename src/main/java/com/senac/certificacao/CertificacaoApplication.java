dd6c08b09c9b6b2ca961c98c22ca0229eb5df2b5
rqpedro <queirooz@hotmail.com>
2026-08-09 09:37:49 -0300

    Camada de tratamento de exceções

--- src/main/java/com/senac/certificacao/CertificacaoApplication.java
+++ src/main/java/com/senac/certificacao/CertificacaoApplication.java
@@ -1,9 +1,5 @@
 package com.senac.certificacao;
 
-
-import dto.UsuarioDTO;
-import entities.Perfil;
-import entities.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
